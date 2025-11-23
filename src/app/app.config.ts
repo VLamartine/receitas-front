86668650c57cdd7bc7d1545c6793c055d82f4c96
Vitor Lamartine <vlamartine@symplicity.com>
2025-11-22 21:29:49 -0300

    Correções para datas e localização

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from "@angular/core";
@@ -8,16 +9,39 @@ import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { authInterceptor } from "@interceptors/auth-interceptor";
-import { provideDateFnsAdapter } from "@angular/material-date-fns-adapter";
-import { ptBR } from "date-fns/locale";
-import { MAT_DATE_LOCALE } from "@angular/material/core";
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from "@angular/material/core";
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
 import { errorInterceptor } from "@interceptors/error-interceptor";
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  provideMomentDateAdapter,
+} from "@angular/material-moment-adapter";
+import "moment/locale/pt-br";
+import localePt from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
+export const MY_FORMATS = {
+  parse: {
+    dateInput: "DD/MM/YYYY",
+  },
+  display: {
+    dateInput: "DD/MM/YYYY",
+    monthYearLabel: "MMMM YYYY",
+    dateA11yLabel: "LL",
+    monthYearA11yLabel: "MMMM YYYY",
+  },
+};
 
+registerLocaleData(localePt);
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideDateFnsAdapter(),
-    { provide: MAT_DATE_LOCALE, useValue: ptBR },
+    { provide: LOCALE_ID, useValue: "pt-BR" },
+    { provide: MAT_DATE_LOCALE, useValue: "pt-br" },
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: MY_FORMATS,
+    },
+    provideMomentDateAdapter(MY_FORMATS),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
